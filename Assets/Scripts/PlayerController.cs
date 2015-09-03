c5c11b5016c77ccaa59bb97f3b75d609d7fbd6a0
eewano <eewano@gmail.com>
2015-09-03 09:44:12 +0900

    1面と2面の迷路を作成。

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 3.0F;
+	public float rotationSpeed = 50.0F;
+
+	void FixedUpdate() {
+		float translation = Input.GetAxis("Vertical") * speed;
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate(0, 0, translation);
+		transform.Rotate(0, rotation, 0);
+	}
+}
