7db078647adecd761c2c16758be0b35a7be2017e
eewano <eewano@gmail.com>
2015-10-05 08:02:57 +0900

    SpawnPointを追加。 1面の迷路の壁の作り直し。Scale2×2×2の大きさのブロック壁を並べる仕様に変更。

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (Player, transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
