f385146b24fb8981267a7b81e09214bd1049debb
eewano <eewano@gmail.com>
2015-10-05 10:51:28 +0900

    Unitychanを追加。アニメーションを設定中。

---
+++ Assets/Scripts/MoveAnime.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAnime : MonoBehaviour {
+
+	Animator animator;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// ここで同期。
+		animator.SetFloat ("Speed", agent.velocity.sqrMagnitude);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,27 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+
+	private NavMeshAgent m_agent;
+	private int SpeedId;
 	// 歩行スピード
 	public float speed = 3.0F;
 	// 方向転換のスピード
 	public float rotationSpeed = 50.0F;
 
-	void FixedUpdate() {
+	// Use this for initialization
+	void Start () {
+		m_agent = this.GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
 		float translation = Input.GetAxis("Vertical") * speed;
 		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 		translation *= Time.deltaTime;
 		rotation *= Time.deltaTime;
 		transform.Translate(0, 0, translation);
-		transform.Rotate(0, rotation, 0);
+		transform.Rotate(0, rotation, 0);	
 	}
 }
