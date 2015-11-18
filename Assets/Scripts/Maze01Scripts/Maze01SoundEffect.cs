4e63c6335fd647a07f53308bf3767f9d01bdec5a
YasutakaAwano <yasutakaawano@YasutakaAwano-no-MacBook-Pro.local>
2015-11-18 19:51:34 +0900

    1.プレイヤーの動きを設定。 2.タイトル画面を作成。 3.1面の画面構成を作成中。Scriptが現在、不完全。

---
+++ Assets/Scripts/Maze01Scripts/Maze01SoundEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maze01SoundEffect : MonoBehaviour {
+	
+	private AudioSource Maze01BGM;
+	private AudioSource ClearSE;
+
+	void Start()
+	{
+		AudioSource[] audioSources = GetComponents<AudioSource> ();
+		Maze01BGM = audioSources [0];
+		ClearSE = audioSources [1];
+	}
+
+	public void Maze01GameStart() {
+		Maze01BGM.PlayOneShot (Maze01BGM.clip);
+	}
+
+	public void Maze01Clear() {
+		Destroy(Maze01BGM);
+		ClearSE.PlayOneShot (ClearSE.clip);
+	}
+}
---
+++ Assets/Scripts/Maze01Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	CharacterController controller;
+	Animator animator;
+
+	Vector3 moveDirection = Vector3.zero;
+
+	public float gravity;
+	public float ForwardSpeed;
+	public float BackwardSpeed;
+	public float RotSpeed;
+	public float JumpPower;
+
+	void Start()
+	{
+		controller = GetComponent<CharacterController> ();
+		animator = GetComponent<Animator> ();
+	}
+		
+	void Update()
+	{
+		if (controller.isGrounded) {
+			if (Input.GetAxis ("Vertical") > 0.0f) {
+				moveDirection.z = Input.GetAxis ("Vertical") * ForwardSpeed;
+			} else if (Input.GetAxis ("Vertical") < 0.0f) {
+				moveDirection.z = Input.GetAxis ("Vertical") * BackwardSpeed;
+			} else {
+				moveDirection.z = 0;
+			}
+
+			transform.Rotate (0, Input.GetAxis ("Horizontal") * RotSpeed, 0);
+
+			if (Input.GetButtonDown ("Jump")) {
+				moveDirection.y = JumpPower;
+				animator.SetTrigger ("Jump");
+			}
+		}
+
+		moveDirection.y -= gravity * Time.deltaTime;
+
+		Vector3 globalDirection = transform.TransformDirection (moveDirection);
+		controller.Move (globalDirection * Time.deltaTime);
+
+		if (controller.isGrounded)
+			moveDirection.y = 0;
+
+		animator.SetBool ("Run", moveDirection.z > 0.0f);
+		animator.SetBool ("Back", moveDirection.z < 0.0f);
+	}
+}
