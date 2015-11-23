fe5088a07093c901f4fe81ef05d3689e55a78c1c
YasutakaAwano <yasutakaawano@YasutakaAwano-no-MacBook-Pro.local>
2015-11-23 14:57:51 +0900

    1.ゲームオーバー画面が完成。

--- Assets/Scripts/Maze01Scripts/Warp.cs
+++ Assets/Scripts/Maze01Scripts/Warp.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Warp : MonoBehaviour {
 
-	Vector3 WarpPoint = new Vector3(-15, 0.08f, -5);
+	Vector3 WarpPoint = new Vector3(-15, 0.08f, -4);
 
 	void OnTriggerEnter(Collider col) {
 		//Playerと接触したら（-15,-0.38,0）の座標までワープする
