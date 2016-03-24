6ed57408237fb36341959ad4f593813f93b5ff28
YasutakaAwano <eewano@gmail.com>
2016-03-24 17:20:09 +0900

    1: マップアイテムのオブジェクトを変更。 2: マップの自動生成システムを設定中。

---
+++ Assets/Mz01FloorCreator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mz01FloorCreator : MonoBehaviour {
+
+	public GameObject prefab = null;
+
+    void Awake()
+    {
+        for(int i = 0; i < 24; i++)
+        {
+            for(int j = 0; j < 48; j += 2)
+            {
+                GameObject mzFloor = Instantiate(prefab) as GameObject;
+                mzFloor.transform.position = new Vector3(
+                        -23.0f + j,
+                        -0.25f,
+                        23.0f - (2 * i));
+            }
+        }
+    }
+}
---
+++ Assets/Mz01WallCreator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mz01WallCreator : MonoBehaviour {
+
+    public GameObject prefab = null;
+
+    void Awake()
+    {
+        GameObject mzWall = Instantiate(prefab) as GameObject;
+        mzWall.transform.position = new Vector3(-21.0f, 1.0f, 21.0f);
+    }
+}
