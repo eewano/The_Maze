8ec6f1ae5e5456bfdfb21bc222531f64c92d3ae2
YasutakaAwano <eewano@gmail.com>
2016-07-13 21:28:19 +0900

    1: プレイヤーがワープする際のシステムを大幅改良。 2: Mz03面の扉を作成。現在、アニメーションを作成中。

---
+++ Assets/Scripts/MzObjScript/MzWarpPoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MzWarpPoint : MonoBehaviour {
+
+    [SerializeField]
+    private float posX;
+    [SerializeField]
+    private float posZ;
+    private float posY = 0.5f;
+    private Mgr_PlayerWarp mgrPlayerWarp;
+
+    void Awake() {
+        mgrPlayerWarp = GameObject.FindWithTag("Player").GetComponent<Mgr_PlayerWarp>();
+    }
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "Player")
+        {
+            mgrPlayerWarp.WarpStartTo(posX, posY, posZ);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript/Mgr_PlayerWarp.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Mgr_PlayerWarp : MonoBehaviour {
+
+    public void WarpStartTo(float x, float y, float z) {
+        transform.position = new Vector3(x, y, z);
+    }
+}
